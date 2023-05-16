f359ed8ed1076e36721aefc2870e18e9ba734908
gabriel <gabrielbarroncas1@gmail.com>
2023-05-16 19:28:34 +0100

    create a function to tags

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -24,6 +24,46 @@ class NotesController{
 
         return response.json()
     }
+
+    async index(request, response){
+        const {title, user_id, tags} = request.query
+
+        let notes
+
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim())
+            notes = await knex("movies_tags")
+            .select([
+                "movies_notes.id",
+                "movies_notes.title",
+                "movies_notes.user_id",
+            ])
+            .where("movies_notes.user_id", user_id)
+            .whereLike("movies_notes.title", `%${title}%`)
+            .whereIn("name", filterTags)
+            .innerJoin("movies_notes", "movies_notes.id", "movies_tags.note_id")
+            .orderBy("movies_notes.title")
+        } else {
+            notes = await knex("movies_notes")
+            .where({user_id})
+            .whereLike("title", `%${title}%`) 
+            .orderBy("title")
+        }
+
+        const userTags = await knex("movies_tags").where({user_id})
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id == note.id)
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
+
+        return response.json(notesWithTags)
+
+    }
 }
 
 module.exports = NotesController
