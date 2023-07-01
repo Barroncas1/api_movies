0cf4f59519a48bc0527568d761f75f412b73b819
gabriel <gabrielbarroncas1@gmail.com>
2023-07-01 15:17:40 +0100

    falta terminar muitas coisas ahhhhhhhhhhhh

---
+++ src/configs/auth.js
@@ -0,0 +1,5 @@
+module.exports = { 
+    jwt: {secret: "default",
+    expiresIn: "1d"
+}
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,19 @@
 const { Router } = require("express")
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
 
 const UserController = require("../controller/UserController")
+const UsersAvatarController = require("../controllers/UsersAvatarController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const userRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
+
 const userControlls = new UserController()
+const usersAvatarController = new UsersAvatarController()
+
 
 userRoutes.post("/", userControlls.create)
-userRoutes.put("/:id", userControlls.update)
-
+userRoutes.put("/", ensureAuthenticated, userControlls.update)
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update)
 module.exports = userRoutes
